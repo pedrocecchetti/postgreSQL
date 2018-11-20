bf9c877208727941c9cadb7bf0c205d0b1b573fc
cecchetti <pedro.cecchetti@gmail.com>
2018-11-20 08:43:23 -0200

    change queries to psycopg2 commands

--- pythondb.py
+++ pythondb.py
@@ -31,19 +31,21 @@ for i in range(0,len(most_viewed_articles)):
 
 
 # Now it creates a view inside the database with the Slugs and the count per slugs
-'''create view slug_counts as 
+c.execute('''create view slug_counts as 
           select substring(path,10), count(status) as num 
           from log 
           group by path 
-          order by num desc offset 1 limit 10;'''
-#Now it extracts a Table with The SUM OF views By author ID
-'''create view authors_counts as 
+          order by num desc offset 1 limit 10;''')
+
+#Now it creates a view extracts a Table with The SUM OF views By author ID
+c.execute('''create view authors_counts as 
           select articles.author as author, sum(slug_counts.num) as freq from
           slug_counts join articles on slug_counts.substring = articles.slug  
           group by author 
-          order by author;'''
+          order by author;''')
 
 #This query create the Table that answer the 2nd question.
-'''select authors.name, authors_count.freq from
+c.execute('''select authors.name, authors_count.freq from
     authors join authors_count on 
-    authors_count.author = authors.id;'''
+    authors_count.author = authors.id;''')
+
